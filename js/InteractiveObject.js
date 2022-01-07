d13799bc327b47092ca3ea0bd2ebc4bda7dc3856
hamiltune <hamiltunemusic@gmail.com>
2022-01-07 13:19:07 -0500

    Added deletion capabilities, resizing with aspect is in progress (without aspect works)

--- js/InteractiveObject.js
+++ js/InteractiveObject.js
@@ -1,15 +1,16 @@
 export default class InteractiveObject {
 
-    constructor(DOMElem, DOMparentElem) {
+    constructor(DOMElem, translatable, resizable, DOMparentElem) {
         this.DOMElem = DOMElem;
         this.DOMparentElem = DOMparentElem;
 
         this.attributes = {};
         // modifiable
-        this.attributes.editable = true;
-        this.attributes.translatable = true;
-        this.attributes.resizable = true;
-        this.attributes.restrictBoundary = true;
+        this.attributes.editable = false; // start uneditable
+        this.attributes.translatable = translatable;
+        this.attributes.resizable = resizable;
+        this.attributes.restrictBoundary = DOMparentElem != null;
+        this.attributes.lockAspectRatio = true;
         this.attributes.resizeBorderWidth = 4;
         this.onDelete = null;
         // internal
@@ -17,6 +18,9 @@ export default class InteractiveObject {
         this.attributes.translating = false;
         this.attributes.resizing = false;
         this.attributes.resizeDir = 0;
+        this.attributes.aspectLocked = false;
+        this.attributes.minWidth = 15;
+        this.attributes.minHeight = 15;
 
         // Ensure position is absolute
         this.DOMElem.style.position = "absolute";
@@ -24,6 +28,39 @@ export default class InteractiveObject {
         this.DOMElem.style.userSelect = "none";
 
         let alias = this;
+
+        // Delete button
+        let deleteBtnScale = 2, deleteBtnSiz = 11;
+        this.deleteBtn = document.createElement("div");
+        this.deleteBtn.style.width = deleteBtnSiz + "px";
+        this.deleteBtn.style.height = deleteBtnSiz + "px";
+        this.deleteBtn.style.marginLeft = deleteBtnSiz / 2 + "px";
+        this.deleteBtn.style.transform = "scale("+deleteBtnScale+")";
+        this.deleteBtn.style.backgroundColor = "red";
+        this.deleteBtn.style.borderRadius = "5px";
+        this.deleteBtn.style.display = "inline-flex";
+        this.deleteBtn.style.alignItems = "center";
+        this.deleteBtn.style.justifyContent = "center";
+        this.bars = [];
+        for (var i = 0; i < 2; i++) {
+            let aBar = document.createElement("div");
+            aBar.style.padding = "0px";
+            aBar.style.margin = "0px";
+            aBar.style.width = "15%";
+            aBar.style.height = "70%";
+            aBar.style.backgroundColor = "white";
+            aBar.style.position = "absolute";
+            aBar.style.transformOrigin = "center center";
+            aBar.style.marginLeft = "0.2px";
+            this.bars.push(aBar);
+        }
+        this.bars[0].style.transform = "rotate(45deg)";
+        this.bars[1].style.transform = "rotate(135deg)";
+        for (var i = 0; i < 2; i++)
+            this.deleteBtn.appendChild(this.bars[i]);
+
+        this.deleteBtn.addEventListener("click", ()=>alias.delete());
+
         this.onMD = function (e) {
             if (!alias.attributes.editable)
                 return;
@@ -184,50 +221,99 @@ export default class InteractiveObject {
         let myRect = this.DOMElem.getBoundingClientRect();
         let useBoundary = this.attributes.restrictBoundary;
 
+       
         let parentRect;
         if (useBoundary) 
             parentRect = this.DOMparentElem.getBoundingClientRect();
 
         let result = {x:myRect.x,y:myRect.y,width:myRect.width,height:myRect.height};
 
+        if ( this.attributes.aspectLocked || this.attributes.lockAspectRatio) {
+            // TODO
+            if (dir == 1 || dir == 4 || dir == 5) {
+                if (!useBoundary || (mousePos.x > parentRect.x && mousePos.x < myRect.left)) {
+                    result.x = mousePos.x;
+                    result.width += myRect.x - mousePos.x;
+                    result.height += myRect.x - mousePos.x;
+                }
+                else if (mousePos.x > myRect.x) {
+                    if (myRect.width > this.attributes.minWidth) {
+                        result.x = mousePos.x;
+                        result.width -= mousePos.x - myRect.x;
+                        result.height -= mousePos.x - myRect.x;
+                    }
+                    else
+                        result.width = this.attributes.minWidth
+                }
+            }
+            return result;
+        }
+
         if (dir == 1 || dir == 4 || dir == 5) {
-            if (!useBoundary || mousePos.x > parentRect.x) {
+            if (!useBoundary || mousePos.x > parentRect.x && mousePos.x < myRect.left) {
                 result.x = mousePos.x;
                 result.width += myRect.x - mousePos.x;
             }
             else if (mousePos.x > myRect.x) {
-                result.x = mousePos.x;
-                result.width -= mousePos.x - myRect.x;
+                if (myRect.width > this.attributes.minWidth) {
+                    result.x = mousePos.x;
+                    result.width -= mousePos.x - myRect.x;
+                }
+                else
+                    result.width = this.attributes.minWidth
             }
         }
         if (dir == 2 || dir == 3 || dir == 6) {
-            if (!useBoundary || mousePos.x < parentRect.right) {
+            if (!useBoundary || mousePos.x < parentRect.right && mousePos.x > myRect.right) {
+                // console.log(myRect.width +","+ this.attributes.minWidth)
                 result.width += mousePos.x - myRect.right;
             }
             else if (mousePos.x < myRect.right) {
-                result.width -= myRect.right - mousePos.x;
+                if (myRect.width > this.attributes.minWidth) {
+                    result.width -= myRect.right - mousePos.x;
+                }
+                else
+                result.width = this.attributes.minWidth;
             }
         }
 
         if (dir == 1 || dir == 3 || dir == 7) {
-            if (!useBoundary || mousePos.y > parentRect.top) {
+            if (!useBoundary || mousePos.y > parentRect.top && mousePos.y < myRect.top) {
                 result.y = mousePos.y;
                 result.height += myRect.y - mousePos.y;
             }
             else if (mousePos.y > myRect.y) {
-                result.y = mousePos.y;
-                result.height -= mousePos.y - myRect.y;
+                if (myRect.height > this.attributes.minHeight) {
+                    result.y = mousePos.y;
+                    result.height -= mousePos.y - myRect.y;
+                }
+                else
+                    result.height = this.attributes.minHeight;
             }
         }
         if (dir == 2 || dir == 4 || dir == 8) {
-            if (!useBoundary || mousePos.y < parentRect.bottom) {
+            if (!useBoundary || mousePos.y < parentRect.bottom && mousePos.y > myRect.bottom) {
                 result.height += mousePos.y - myRect.bottom;
             }
             else if (mousePos.y < myRect.bottom) {
-                result.height -= myRect.bottom - mousePos.y;
+                if (myRect.height > this.attributes.minHeight) {
+                    result.height -= myRect.bottom - mousePos.y;
+                }
+                else
+                    result.height = this.attributes.minHeight;
             }
         }
 
         return result;
+    }
+
+    toggleEdit() {
+        if (!this.attributes.editable) {
+            this.DOMElem.appendChild(this.deleteBtn);
+        }
+        else {
+            this.DOMElem.removeChild(this.deleteBtn);
+        }
+        this.attributes.editable = !this.attributes.editable;
     }
 }
